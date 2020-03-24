8908b34ffb307031d6ad599abf113b7e0e832790
Henry Le Grys <henry@legrys.info>
2020-03-24 02:51:16 +0000

    Start sidewinder project & identity app

---
+++ sidewinder/identity/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.forms import UserChangeForm, AdminPasswordChangeForm
+from django.urls import path
+
+from sidewinder.identity.models import User, RedditCredentials
+
+class IdentityUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form = IdentityUserChangeForm
+    list_display = ('username', 'uid', 'pronouns',)
+    readonly_fields = ('uid',)
+    change_password_form = AdminPasswordChangeForm
+    change_user_password_template = None
+
+    fieldsets = (
+        ('User details', {
+            "fields": ('username', 'uid', 'password', 'date_joined',)
+        }),
+        ('Profile', {
+            "fields": ('email', 'pronouns',)
+        }),
+        ('Permissions', {
+            "fields": ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)
+        }),
+    )
+
+    def get_urls(self):
+        return [
+            path('<id>/password/',
+                 self.admin_site.admin_view(auth_admin.UserAdmin.user_change_password.__get__(self, self.__class__)),
+                 name='auth_user_password_change')
+        ] + super().get_urls()
+
+    def lookup_allowed(self, lookup, value):
+        # Don't allow lookups involving passwords.
+        return not lookup.startswith('password') and super().lookup_allowed(lookup, value)
+
+@admin.register(RedditCredentials)
+class RedditCredentialsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'last_refresh',)
---
+++ sidewinder/identity/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+from django.utils import timezone
+
+
+class UserMixin(models.Model):
+    is_staff = models.BooleanField(verbose_name='staff status', default=False,
+                                   help_text="Designates whether the user can log into this admin site.")
+    is_active = models.BooleanField(verbose_name='active', default=True,
+                                    help_text="Designates whether this user should be treated as active. "
+                                              "Unselect this instead of deleting accounts.")
+    date_joined = models.DateTimeField(verbose_name='date joined', default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractBaseUser, PermissionsMixin, UserMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    uid = models.CharField(max_length=32, primary_key=True, help_text="Reddit ID")
+    username = models.CharField(max_length=100, unique=True,
+                                help_text="Required. 100 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                                validators=[username_validator],
+                                error_messages={
+                                    'unique': "A user with that username already exists.",
+                                })
+    email = models.EmailField(verbose_name='email address', blank=True)
+    pronouns = models.CharField(max_length=300, default='unspecified')
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['uid', 'email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+class RedditCredentials(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tokens')
+
+    access_token = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=200)
+    last_refresh = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} - Reddit"
+
+    class Meta:
+        verbose_name = 'Reddit Credentials'
+        verbose_name_plural = 'Reddit Credentials'
