61080a5b44c6b2a7871af6c18474fd5243bc8561
Henry Le Grys <henry@legrys.info>
2020-03-29 03:47:23 +0100

    Automatically create user if missing on reddit signin & messaging

--- sidewinder/identity/views.py
+++ sidewinder/identity/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponseRedirect, JsonResponse, HttpResponse
 from django.urls import reverse
 from django.utils import timezone
@@ -21,6 +22,9 @@ def reddit_login(request: HttpRequest):
     state = generate_state_token()
     redirect_url = reddit.auth.url(['identity'], state)
 
+    if 'return_to' in request.GET:
+        request.session['return_to'] = request.GET['return_to']
+
     request.session['state'] = state
 
     return HttpResponseRedirect(redirect_url)
@@ -28,14 +32,22 @@ def reddit_login(request: HttpRequest):
 
 def authorize_callback(request: HttpRequest):
     reddit = _build_reddit(request)
+    redirect_to = "/"
+
+    if 'return_to' in request.session:
+        redirect_to = request.session['return_to']
+    redirect_to = request.build_absolute_uri(redirect_to)
 
     if 'error' in request.GET:
         error_msg = request.GET['error']
+        messages.error(request, f"Couldn't sign you in with Reddit: {error_msg}")
 
-        return  # TODO: handle error
+        return HttpResponseRedirect(redirect_to)
 
     if request.GET['state'] != request.session['state']:
-        return JsonResponse({"error": "invalid state"})
+        messages.error(request, "Couldn't sign you in with Reddit: invalid state parameter")
+
+        return HttpResponseRedirect(redirect_to)
 
     code = request.GET['code']
     refresh_token = reddit.auth.authorize(code)
@@ -43,22 +55,23 @@ def authorize_callback(request: HttpRequest):
 
     try:
         user = User.objects.get(uid=reddit_user.id)
-        login(request, user)
-
-        # noinspection PyProtectedMember
-        access_token = reddit._authorized_core._authorizer.access_token
-
-        creds, created = RedditCredentials.objects.get_or_create(
-            user=user,
-            defaults=dict(access_token=access_token, refresh_token=refresh_token, last_refresh=timezone.now())
-        )
-
-        if not created:
-            creds.access_token = access_token
-            creds.refresh_token = refresh_token
-            creds.last_refresh = timezone.now()
-            creds.save()
     except User.DoesNotExist:
-        pass  # TODO: auto sign up
+        user = User.objects.create_user(reddit_user.name, uid=reddit_user.id)
 
-    return HttpResponse(b"success, probably")
+    login(request, user)
+
+    # noinspection PyProtectedMember
+    access_token = reddit._authorized_core._authorizer.access_token
+
+    creds, created = RedditCredentials.objects.get_or_create(
+        user=user,
+        defaults=dict(access_token=access_token, refresh_token=refresh_token, last_refresh=timezone.now())
+    )
+
+    if not created:
+        creds.access_token = access_token
+        creds.refresh_token = refresh_token
+        creds.last_refresh = timezone.now()
+        creds.save()
+
+    return HttpResponseRedirect(redirect_to)
