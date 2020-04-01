3b7b4abfa2f83e9b048ea5fc1bb0b0ae88d838bf
Henry Le Grys <henry@legrys.info>
2020-04-01 23:46:56 +0100

    Add search bar & filters to answers admin

--- april/imposter/admin.py
+++ april/imposter/admin.py
@@ -6,3 +6,5 @@ from april.imposter.models import KnownAnswer
 @admin.register(KnownAnswer)
 class KnownAnswerAdmin(admin.ModelAdmin):
     list_display = ('pk', 'message', 'correct', 'seen_times', 'updated_at',)
+    search_fields = ('message',)
+    list_filter = ('correct',)
