80e1363f652cced477b0aea647a008fc6b1e3373
Henry Le Grys <henry@legrys.info>
2020-04-13 03:22:05 +0100

    Remove gremlins-api (2020 reddit event) from the CORS origin whitelist

--- config/production.py
+++ config/production.py
@@ -1,6 +1,5 @@
 # Django production config file
 
-from .base import *
 import environ
 import sentry_sdk
 
@@ -25,7 +24,6 @@ STATIC_ROOT = env('STATIC_ROOT', default="static/")
 CORS_ORIGIN_WHITELIST = [
     "https://snakeroom.org",
     "https://api.snakeroom.org",
-    "https://gremlins-api.reddit.com",
 ]
 
 ALLOWED_HOSTS = CSRF_TRUSTED_ORIGINS = [
