8a1c897a52a8cb05f7679a6ac0aefa768b8e284e
Henry Le Grys <henry@legrys.info>
2022-03-30 22:25:07 +0100

    Add a content field to project divisions

--- april/place/models.py
+++ april/place/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from sidewinder.identity.models import User
 
+import struct
 import uuid
 
 class Project(models.Model):
@@ -15,10 +16,39 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
+def default_division_bytes():
+    # pos 0, 0; size 0, 0; no data
+    return b"\x00\x00\x00\x00\x00\x00\x00\x00"
+
 class ProjectDivision(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     priority = models.IntegerField(help_text="Lower is better")
+    content = models.BinaryField(help_text="Image data", editable=False, default=default_division_bytes)
+
+    # content is big-endian 4 shorts header followed by bytearray of data
 
     def __str__(self):
         return f"{self.project.name} - {self.uuid}"
+
+    def unpack_header(self):
+        return struct.unpack(">HHHH", self.content)
+
+    def get_origin(self) -> (int, int):
+        """
+        Get the origin of this layer
+        :return: Position tuple (x, y)
+        """
+        pos = self.unpack_header()[:2]
+        return pos[0], pos[1]
+
+    def get_dimensions(self) -> (int, int):
+        """
+        Get the size of this layer
+        :return: Size tuple (width, height)
+        """
+        size = self.unpack_header()[2:]
+        return size[0], size[1]
+
+    def get_image_bytes(self) -> bytes:
+        return self.content[4:]
