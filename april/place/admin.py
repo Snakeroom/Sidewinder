caddb04d44fc232caf464c3e1339ca5a074b2c08
Melissa🦒 <Melissa0x1f992@gmail.com>
2022-03-31 21:30:35 -0500

    Change high priority to featured, add project approved and division enabled to admin panel

--- april/place/admin.py
+++ april/place/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from .models import Project, ProjectDivision
 
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'uuid', 'high_priority',)
-    list_filter = ('high_priority',)
+    list_display = ('name', 'uuid', 'featured', 'approved')
+    list_filter = ('featured',)
+
 
 @admin.register(ProjectDivision)
 class ProjectDivisionAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'priority',)
+    list_display = ('__str__', 'priority', 'enabled')
